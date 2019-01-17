4ff5a083e43c1598eb9d9e9fb5c998a73f8b8155
lss <404629780@qq.com>
2019-01-17 17:32:06 +0800

    redission的并发测试

--- redisson/src/main/java/com/example/redisson/controller/TestController.java
+++ redisson/src/main/java/com/example/redisson/controller/TestController.java
@@ -1,7 +1,14 @@
 package com.example.redisson.controller;
 
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description 测试
@@ -10,9 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class TestController {
+    @Autowired
+    private RedissonClient redissonClient;
 
-    @GetMapping("/test")
-    public String test(){
-        return "hello world";
+    ExecutorService executorService= Executors.newFixedThreadPool(10);
+    private static int num=100;
+
+    @GetMapping("/lock")
+    public String lockTest(){
+        num=1000;
+        for (int i=0;i<10;i++){
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    locks();
+                }
+            });
+        }
+        return "ok";
     }
+
+
+    public void locks() {
+        while (num>0){
+            RLock rlock = redissonClient.getLock("lss_lock");
+            //设置锁超时时间，防止异常造成死锁
+            rlock.lock(20, TimeUnit.SECONDS);
+            try{
+                //执行业务逻辑
+                if(num>0){
+                    System.out.println("抢到第"+num--+"个");
+                }
+            } catch(Exception e){
+            }finally{
+                rlock.unlock();
+            }
+        }
+    }
+
 }
---
+++ redisson/src/main/java/com/example/redisson/redis/RedissonConfig.java
@@ -0,0 +1,40 @@
+package com.example.redisson.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description 配置
+ * @Athor lss0555
+ * @Date 2019/1/15/015 16:18
+ **/
+@Component
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+
+    @Bean
+    public RedissonClient getRedisson(){
+
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setPassword(password);
+        //添加主从配置
+//        config.useMasterSlaveServers().setMasterAddress("").setPassword("").addSlaveAddress(new String[]{"",""});
+        return Redisson.create(config);
+    }
+
+}
